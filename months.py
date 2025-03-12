d8566ed09ca88d01c136fe60f5e9d3d32a76a412
anjalikagitha <kagithanagaanjali@gmail.com>
2025-03-12 16:11:59 +0530

    Create months.py

---
+++ months.py
@@ -0,0 +1,11 @@
+months=int(input("enter a month number :"))
+if months==12 or months==1 or months==2:
+    print("It's a Winter Season!")
+elif months==3 or months==4 or months==5:
+    print("It's a Summer Season!") 
+elif months==6 or months==7 or months==8:
+    print("It's a Rainy Season!")  
+elif months==9 or months==10 or months==11:
+    print("It's a Spring Season!")
+else: 
+    print("Please enter correct mont number friend!")    
