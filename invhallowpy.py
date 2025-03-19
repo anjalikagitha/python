bfc36578236844f0a54269f3a9fccf475b87600d
anjalikagitha <kagithanagaanjali@gmail.com>
2025-03-19 21:39:16 +0530

    Create invhallowpy.py

---
+++ invhallowpy.py
@@ -0,0 +1,12 @@
+n=7
+
+for i in range(n):
+  if i==(n-3):
+    print((n-1)*" "+"*")
+  elif i==0:
+    print("* "*n)  
+
+  elif i==1:
+    print("  "*i+"*"+"  "*(n-4)+" *")  
+  elif i==2:
+     print("  "*i+"*"+" "*(n-5)+" *")  
